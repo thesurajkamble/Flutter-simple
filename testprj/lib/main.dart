1323efd1d8d0116836f4d3b348c5dca3ceb547ab
surajkamblesk <alphacoderofficial@gmail.com>
2019-12-23 04:24:48 +0530

    scaffold concept

--- testprj/lib/main.dart
+++ testprj/lib/main.dart
@@ -1,10 +1,11 @@
-import 'package:flutter/material.dart';
-// Uncomment lines 7 and 10 to view the visual layout at runtime.
-// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
-
+import 'package:flutter/material.dart' ;
 void main() {
   // debugPaintSizeEnabled = true;
   runApp(MaterialApp(
-    home: Text("HEY this is suraj"),
+    home:Scaffold(
+      appBar: AppBar(
+        title: Text("My appie"),
+      ),
+    )
   ));
 }
